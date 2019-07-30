80085ce5a81813bb9cc5b93d0697dc6149c03ac5
Jane <Jane@jingdigital.com>
2019-07-30 19:54:24 +0800

    testng suite

--- Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
+++ Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
@@ -1,8 +1,41 @@
 package com.course.testng;
 
+import org.testng.annotations.*;
+
 public class BasicAnnotation {
     
-
+    @Test
+    public void testCase1(){
+        System.out.println("这是测试test1");
+    }
+    @Test
+    public void testCase2(){
+        System.out.println("这是测试test2");
+    }
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("beforeMethod这是");
+    }
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("afterMethod这是");
+    }
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("beforeClass这是类运行之前的方法");
+    }
+    @AfterClass
+    public void afterClass(){
+        System.out.println("afterClass这是类运行之后的方法");
+    }
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("afterSuite");
+    }
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("beforeSuite");
+    }
 
 
 }
---
+++ Chapter5/src/main/java/com/course/testng/suite/LoginTest.java
@@ -0,0 +1,18 @@
+package com.course.testng.suite;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    @Test
+    public void payTest() {
+        System.out.println("Logintest这是");
+    }
+
+    @Test(expectedExceptions = ArithmeticException.class)
+    public void divisionWithException()
+    {
+        int i = 1 / 0;
+        System.out.println("After division the value of i is :"+ i);
+    }
+
+}
